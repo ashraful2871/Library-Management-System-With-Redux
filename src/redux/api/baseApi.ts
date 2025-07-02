c17a1ad408b98ee4e8f0e7f55a043cf3f6cef788
ashraful2871 <ashrafulislam2758@gmail.com>
2025-07-02 22:18:32 +0600

    add local server link for testing

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://assignment-3-inky-eight.vercel.app/api",
+    baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["book"],
   endpoints: (builder) => ({
